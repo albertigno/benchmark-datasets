792641dbe3d0eaba246cdb6f65cbf91a29b09fa9
albertigno <albertigno1129@gmail.com>
2019-01-29 11:43:42 +0100

    first commit

---
+++ cifar10_coolest.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Simple CNN for the CIFAR10 Dataset
+import numpy
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras.constraints import max_norm
+from keras import backend as K
+K.set_image_dim_ordering('th')
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# reshape to be [samples][channels][width][height]
+X_train = X_train.reshape(X_train.shape[0], 3, 32, 32).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 3, 32, 32).astype('float32')
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+# define a simple CNN model
+def baseline_model():
+    # create model
+    model = Sequential()
+    model.add(Convolution2D(5, 5, 5, input_shape=(3, 32, 32), kernel_initializer= 'glorot_uniform' , kernel_constraint=max_norm(1.), bias_constraint=max_norm(0.4), activation='relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(20,  kernel_initializer= 'glorot_uniform', kernel_constraint=max_norm(1.), bias_constraint=max_norm(0.4), activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+# build the model
+model = baseline_model()
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=10, batch_size=100,
+    verbose=2)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("CNN Error: %.2f%%" % (100-scores[1]*100))
+
+model.save("./cooolest_mnist.h5")
+print("Saved model to disk")
